26b050242dc9cc3deb2322bcac39546e46c7f493
DeITdev <danarikram@gmail.com>
2026-02-22 16:01:16 +0700

    add personal logo

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -6,6 +6,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url,
