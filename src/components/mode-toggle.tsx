92f52ce7fd74abac64e0c8e380d5a305cdefba32
Danar Ikram T. <89527513+DeITdev@users.noreply.github.com>
2026-03-23 02:35:48 +0700

    update toggle mode UI dropdown

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -18,17 +18,17 @@ export function ModeToggle() {
                     <img src="/icons/mode.svg" alt="theme mode" />
                 </button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="center" className="px-2 mt-1.5">
-                <DropdownMenuItem className={`mt-2 gap-4 ${theme === "light" ? "bg-accent text-accent-foreground" : ""}`} onClick={() => setTheme("light")}>
-                    <Sun className={`size-4 ${theme === "light" ? "fill-[#FFC030] text-[#FFC030]" : ""}`} />
+            <DropdownMenuContent align="center" className="px-2 mt-5 bg-white/35 dark:bg-gray-800/35 backdrop-blur-3xl border-none">
+                <DropdownMenuItem className={`mt-2 gap-4 cursor-pointer transition-colors hover:bg-gray-200/60 focus:bg-gray-200/60 dark:hover:bg-white/10 dark:focus:bg-white/10 ${theme === "light" ? "bg-white/50 text-accent-foreground" : ""}`} onClick={() => setTheme("light")}>
+                    <Sun className={`size-4 ${theme === "light" ? "fill-[#FFC030] text-[#FFC030]" : "text-black dark:text-white"}`} />
                     Light
                 </DropdownMenuItem>
-                <DropdownMenuItem className={`my-2 gap-4 ${theme === "dark" ? "bg-accent text-accent-foreground" : ""}`} onClick={() => setTheme("dark")}>
-                    <Moon className={`size-4 ${theme === "dark" ? "fill-[#FF6157] text-[#FF6157]" : ""}`} />
+                <DropdownMenuItem className={`my-2 gap-4 cursor-pointer transition-colors hover:bg-gray-200/60 focus:bg-gray-200/60 dark:hover:bg-white/10 dark:focus:bg-white/10 ${theme === "dark" ? "dark:bg-gray-800/50 text-accent-foreground" : ""}`} onClick={() => setTheme("dark")}>
+                    <Moon className={`size-4 ${theme === "dark" ? "fill-[#FF6157] text-[#FF6157]" : "text-black dark:text-white"}`} />
                     Dark
                 </DropdownMenuItem>
-                <DropdownMenuItem className={`mb-2 gap-4 ${theme === "system" ? "bg-accent text-accent-foreground" : ""}`} onClick={() => setTheme("system")}>
-                    <Monitor className={`size-4 ${theme === "system" ? "fill-[#2acb42] text-[#2acb42]" : ""}`} />
+                <DropdownMenuItem className={`mb-2 gap-4 cursor-pointer transition-colors hover:bg-gray-200/60 focus:bg-gray-200/60 dark:hover:bg-white/10 dark:focus:bg-white/10 ${theme === "system" ? "bg-white/50 dark:bg-gray-800/50 text-accent-foreground" : ""}`} onClick={() => setTheme("system")}>
+                    <Monitor className={`size-4 ${theme === "system" ? "fill-[#2acb42] text-[#2acb42]" : "text-black dark:text-white"}`} />
                     System
                 </DropdownMenuItem>
             </DropdownMenuContent>
